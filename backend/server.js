07447df386982063a7721180cc667334631e4177
NikhilPatel <leakee134@gmail.com>
2025-08-15 11:02:50 +0530

    Prepared backend for render

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "*",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
